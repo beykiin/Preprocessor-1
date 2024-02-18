675c1565e046858585f80c35c8dcfc747ee9b5c8
beykiin <132366582+beykiin@users.noreply.github.com>
2024-02-18 14:21:01 +0300

    Add files via upload

---
+++ Preprocessor 1.cpp
@@ -0,0 +1,70 @@
+﻿// Ön İşlemci 1.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+//#define SAYI 1
+//
+//int main()
+//{
+//    std::cout << SAYI + SAYI;
+//}
+
+//#define SAYI 1
+//#define TOPLA(a,b) (a+b)
+//
+//int main()
+//{
+//    std::cout << TOPLA(10,30);
+//}
+
+//#define DEBUG
+//
+//
+//int main()
+//{
+//#ifdef DEBUG
+//    std::cout << "Big Message";
+//#endif // DEBUG
+//
+//
+//    
+//}
+
+//#define DEBUG
+//
+//
+//int main()
+//{
+//#ifdef DEBUG
+//    std::cout << "Big Message";
+//#endif // DEBUG
+//
+//
+//
+//}
+
+
+
+//int main()
+//{
+//#ifndef DEBUG
+//    std::cout << "Big Message";
+//#endif // DEBUG
+//
+//
+//
+//}
+
+
+
+// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
+// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
+
+// Kullanmaya Başlama İpuçları: 
+//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
+//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
+//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
+//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
+//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
+//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
